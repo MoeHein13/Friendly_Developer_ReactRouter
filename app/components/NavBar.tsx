9cd0f0b2f6cb08d6bf454a3826845f4df2faca8b
Kelvin <kelvinmoe96@gmail.com>
2025-08-22 00:35:20 +0800

    update  the menu bar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,27 +1,112 @@
 import { NavLink } from "react-router";
-import { FaLaptopCode } from "react-icons/fa";
+import { FaLaptopCode, FaBars } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { useState } from "react";
 
 const NavBar = () => {
-  return (
-    <nav className="bg-gray-800 border-gray-400 px-4 py-4 flex justify-between sticky">
-      <NavLink
-        to="/"
-        className="flex items-center space-x-2 text-lg font-bold text-blue-300"
-      >
-        <FaLaptopCode className="text-blue-400 text-xl" />
+  const base = "text-gray-300  ";
+  const active = "text-blue-400 font-bold";
 
-        <span>The Friendly Developer</span>
-      </NavLink>
-      <div className="hidden md:flex items-center gap-6">
-        <div className="space-x-3 text-gray-300 text-sm  ">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/projects">Projects</NavLink>
-          <NavLink to="/blog">Blog</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-        </div>
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClickMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+  const renderResponsiveMenu = (
+    <div className="bg-gray-600 p-4 ">
+      <div className="space-x-3 text-gray-300 text-sm flex justify-between ">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/blog"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Blog
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? active : base)}
+        >
+          Contact
+        </NavLink>
       </div>
-    </nav>
+    </div>
+  );
+  return (
+    <>
+      <nav className="bg-gray-800 border-gray-400 px-4 py-4 flex justify-between sticky items-center">
+        <NavLink
+          to="/"
+          className="flex items-center space-x-2 text-lg font-bold text-blue-300"
+        >
+          <FaLaptopCode className="text-blue-400 text-xl" />
+
+          <span>The Friendly Developer</span>
+        </NavLink>
+        <div className="hidden md:flex items-center gap-6">
+          <div className="space-x-3 text-gray-300 text-sm  ">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              Blog
+            </NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) => (isActive ? active : base)}
+            >
+              Contact
+            </NavLink>
+          </div>
+        </div>
+        {menuOpen ? (
+          <ImCross
+            onClick={handleClickMenu}
+            className="text-blue-400 cursor-pointer"
+          />
+        ) : (
+          <FaBars
+            className=" text-blue-400 cursor-pointer flex  md:hidden"
+            onClick={handleClickMenu}
+          />
+        )}
+      </nav>
+      {menuOpen && renderResponsiveMenu}
+    </>
   );
 };
 
